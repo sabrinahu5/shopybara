e25ddb87e1b0b2bfc8583bbcdd2bcf56ee1343f6
Andrew Chen <andche@ucdavis.edu>
2025-02-15 01:14:53 -0800

    protecting routes

---
+++ app/on-boarding/page.tsx
@@ -0,0 +1,7 @@
+export default function OnBoarding() {
+  return (
+    <div>
+      <h1>On-boarding Page</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function LandingPage() {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900">
       {/* Hero Section */}
